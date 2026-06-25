6e6ce6a009282d751b60bf616cc52269c0026175
Chris22 <80302865+ChrisM2000-hub@users.noreply.github.com>
2026-06-25 18:58:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,13 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tsConfigPaths(),
   ],
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
 })
