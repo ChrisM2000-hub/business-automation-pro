76764fa9678560c6b262ed506d456913bb052425
Chris22 <80302865+ChrisM2000-hub@users.noreply.github.com>
2026-06-25 13:00:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss/vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
+    TanStackRouterVite(),
     react(),
     tailwindcss(),
     tsConfigPaths(),
   ],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
 })
